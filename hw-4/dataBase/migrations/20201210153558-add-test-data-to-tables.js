92735c14b93cdb75e1017d9d22723fc1f56e0f46
OlgaIvashkiv <olgasworking@gmail.com>
2020-12-10 18:11:02 +0200

    hw-5 - added add-test-data-to-tables migration

--- hw-4/dataBase/migrations/20201210153558-add-test-data-to-tables.js
+++ hw-4/dataBase/migrations/20201210153558-add-test-data-to-tables.js
@@ -1,13 +1,19 @@
 module.exports = {
     // eslint-disable-next-line no-unused-vars
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.bulkInsert(
-        'cars', [{
+        await queryInterface.bulkInsert('users', [{
+            id: 1,
+            name: 'Olga',
+            email: 'olgaivashkiv@gmail.com',
+            password: '12345678',
+            age: 27
+        }]);
+
+        await queryInterface.bulkInsert('cars', [{
             model: 'mercedes',
             year: 2011,
             users_id: 1,
-        }]
-);
+        }]);
     },
     // eslint-disable-next-line no-unused-vars
     down: async (queryInterface, Sequelize) => {
